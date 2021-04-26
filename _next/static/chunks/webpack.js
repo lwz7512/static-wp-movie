e1f4e2fe19545151600c0979fa3c7a3242e8adf7
lwz7512 <lwz7512@gmail.com>
2021-04-26 18:58:07 -0300

    fix image lost

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "aec14fc5a4c70343c8a2";
+/******/ 	var hotCurrentHash = "ac698d3e46781d1060ba";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
